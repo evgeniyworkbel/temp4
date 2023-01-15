55843b11b87d6c523056242bd90b94ebaa6ab601
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-01-15 19:12:16 +0300

    Implement routing for "/", "/login", error-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,22 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './routes/error-page';
+import Login from './routes/login';
 import './App.css';
 
-const router = createBrowserRouter({
-
-});
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
---
+++ frontend/src/routes/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      Login Form!
+    </div>
+  );
+}
+
+export default Login;
