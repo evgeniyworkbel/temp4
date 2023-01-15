6d27034bd43b7859eb19d1f7ecaad89af526b658
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-01-15 13:29:11 +0300

    Remove unnecessary code, logo; import react-router-provider

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import '../App.css';
+
+const router = createBrowserRouter({
+
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit src/App.js and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
