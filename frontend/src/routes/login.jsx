002e68906b1a0d6e4a0d2f02e6c66a933cf1fdde
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-01-15 21:47:00 +0300

    Implement login form, add bootstrap styles to src/index.jsx

--- frontend/src/routes/login.jsx
+++ frontend/src/routes/login.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import { Button, Form } from 'react-bootstrap';
 
 function Login() {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
-    <div>
-      Login Form!
-    </div>
+    <>
+      <h1 className="text-center mb-4">Войти</h1>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group className="form-floating mb-3" controlId="username">
+          <Form.Control
+            name="username"
+            type="text"
+            placeholder="Ваш ник"
+            autoComplete="username"
+            required
+            onChange={formik.handleChange}
+            value={formik.values.username}
+          />
+          <Form.Label>Ваш ник</Form.Label>
+        </Form.Group>
+
+        <Form.Group className="form-floating mb-4" controlId="password">
+          <Form.Control
+            name="password"
+            type="text"
+            placeholder="Пароль"
+            autoComplete="current-password"
+            required
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          <Form.Label>Пароль</Form.Label>
+        </Form.Group>
+
+        <Button variant="outline-primary" className="mb-4" type="submit">
+          Войти
+        </Button>
+      </Form>
+    </>
   );
 }
 
