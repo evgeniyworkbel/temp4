e2f7b4032a49109b8d3e4b1acf146118f8ca1aba
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-01-22 19:48:45 +0300

    Lay out root and login page: add navbar, containers, rows, cols and card

--- frontend/src/routes/LoginForm.jsx
+++ frontend/src/routes/LoginForm.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { useFormik } from 'formik';
-import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Form,
+  Button,
+} from 'react-bootstrap';
+
 import { loginFormSchema } from '../schemas/index';
 
 function Login() {
@@ -16,40 +25,63 @@ function Login() {
   });
 
   return (
-    <>
-      <h1 className="text-center mb-4">Войти</h1>
-      <Form onSubmit={formik.handleSubmit}>
-        <Form.Group className="form-floating mb-3" controlId="username">
-          <Form.Control
-            name="username"
-            type="text"
-            placeholder="Ваш ник"
-            autoComplete="username"
-            required
-            onChange={formik.handleChange}
-            value={formik.values.username}
-          />
-          <Form.Label>Ваш ник</Form.Label>
-        </Form.Group>
+    <Container fluid className="h-100">
+      <Row className="h-100 justify-content-center align-content-center">
+        <Col md={8} xxl={6}>
+          <Card className="shadow-sm">
+            <Card.Body className="p-5">
+              <Row>
+                <Col>
+                  <div className="text-center">
+                    <img src="./images/mountain.png" className="rounded-circle" alt="Войти" />
+                  </div>
+                </Col>
+                <Col>
+                  <Form onSubmit={formik.handleSubmit}>
+                    <h1 className="text-center mb-4">Войти</h1>
+                    <Form.Group className="form-floating mb-3" controlId="username">
+                      <Form.Control
+                        name="username"
+                        type="text"
+                        placeholder="Ваш ник"
+                        autoComplete="username"
+                        required
+                        onChange={formik.handleChange}
+                        value={formik.values.username}
+                      />
+                      <Form.Label>Ваш ник</Form.Label>
+                    </Form.Group>
 
-        <Form.Group className="form-floating mb-4" controlId="password">
-          <Form.Control
-            name="password"
-            type="text"
-            placeholder="Пароль"
-            autoComplete="current-password"
-            required
-            onChange={formik.handleChange}
-            value={formik.values.password}
-          />
-          <Form.Label>Пароль</Form.Label>
-        </Form.Group>
+                    <Form.Group className="form-floating mb-4" controlId="password">
+                      <Form.Control
+                        name="password"
+                        type="password"
+                        placeholder="Пароль"
+                        autoComplete="current-password"
+                        required
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                      />
+                      <Form.Label>Пароль</Form.Label>
+                    </Form.Group>
 
-        <Button variant="outline-primary" className="mb-4" type="submit">
-          Войти
-        </Button>
-      </Form>
-    </>
+                    <Button variant="outline-primary" className="mb-4" type="submit">
+                      Войти
+                    </Button>
+                  </Form>
+                </Col>
+              </Row>
+            </Card.Body>
+            <Card.Footer className="p-4">
+              <div className="text-center">
+                <span className="me-1">Нет аккаунта?</span>
+                <Link to="/signup">Регистрация</Link>
+              </div>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Navbar, Container } from 'react-bootstrap';
 
 function Root() {
   return (
-    <div>
-      I am a Root, baby!
+    <div className="d-flex flex-column h-100">
+      <Navbar variant="light" bg="white" expand="lg" className="shadow-sm">
+        <Container>
+          <Navbar.Brand href="/">Chat</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <div id="main" className="h-100">
+        <Outlet />
+      </div>
     </div>
   );
 }
