b11dbcb9ae5250f8450b1039f496ff4c22b76adf
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-01-23 17:01:45 +0300

    Correct imports and filepaths

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
