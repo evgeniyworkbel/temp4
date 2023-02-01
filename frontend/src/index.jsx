12b32e1b4b6985c1b071e9b95e6fbd2d551531a2
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-02-02 01:44:54 +0300

    Put imports in right order

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals.js';
+
+import App from './App.jsx';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
